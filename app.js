97999cbeab36e274accac302a49baead3817ef49
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2024-11-22 01:15:22 +0530

    Bat movement for player1 is done two is in process

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const gameArena = document.querySelector('.game-arena');
     const arenaWidth = 800;
     const arenaHeight = 400;
@@ -6,44 +6,89 @@ document.addEventListener('DOMContentLoaded', function() {
     let scorePlayer1 = 0;
     let scorePlayer2 = 0;
     let gamestarted = false;
-    let ball = {x:200, y:0};
-    let batPlayer1 = [{x:180, y:-10},{x:200, y:-10}, {x:220, y:-10}];
-    let batPlayer2 = [{x:180, y:800},{x:200, y:800}, {x:220, y:800}];
+    let ball = { x: 200, y: 0 };
+    let batPlayer1 = [{ x: 180, y: -10 }, { x: 200, y: -10 }, { x: 220, y: -10 }];
+    let batPlayer2 = [{ x: 180, y: 800 }, { x: 200, y: 800 }, { x: 220, y: 800 }];
 
     let dx = 0;
     let dy = 0;
 
-
     function changeDirection(e) {
         console.log(e);
-        if (e.key === 'ArrowUp') {
+        if (e.key === 'ArrowUp' || e.key === 'KeyQ') {
             dx = -cellSize;
-            dy = 0; 
-        } else if (e.key === 'ArrowDown') {
+            dy = 0;
+        } else if (e.key === 'ArrowDown' || e.key === 'KeyA') {
             dx = cellSize;
-            dy = 0; 
+            dy = 0;
         }
+    }
 
+    function stopBat(e) {
         
+        if (e.key === 'ArrowUp' || e.key === 'KeyQ' || e.key === 'ArrowDown' || e.key === 'KeyA') {
+            dx = 0;
+            dy = 0;
+        }
     }
 
     function updateBat() {
-        console.log(dx);
-        if(batPlayer1[0].x <= 375 && batPlayer1[0].x >= 0) {
-            if (dx > 0) {
-                const newPos = {x: batPlayer1[0].x + dx, y: batPlayer1[0].y};
-                console.log(newPos.x);
-                batPlayer1.push(newPos);
-                batPlayer1.shift();
-                console.log(batPlayer1[0].x)
-            } else if(dx < 0) {
-                const newPos = {x: batPlayer1[0].x + dx, y: batPlayer1[0].y};
-                batPlayer1.unshift(newPos);
-            }
-            
-        }
+        const batLength = 3;
         
+        if (dx !== 0 || dy !== 0) {
+            const newPosPlayer1 = { // Player1
+                x: batPlayer1[0].x + dx, 
+                y: batPlayer1[0].y 
+            };
+
+            const newPosUpPlayer1 = { // Player1
+                x: batPlayer1[batPlayer2.length - 1].x + dx,
+                y: batPlayer1[batPlayer2.length - 1].y
+            };   
+
+            const newPosPlayer2 = { // Player1
+                x: batPlayer2[0].x + dx, 
+                y: batPlayer2[0].y 
+            };
+
+            const newPosUpPlayer2 = { // Player1
+                x: batPlayer2[batPlayer2.length - 1].x + dx,
+                y: batPlayer2[batPlayer2.length - 1].y
+            }; 
+            
+            if (newPosPlayer1.x >= 0 && newPosPlayer1.x <= arenaHeight - cellSize && newPosUpPlayer1.x >= 0 && newPosUpPlayer1.x <= arenaHeight - cellSize) {
+                if (dx !== 0) {
+                    if (dx > 0) {
+                        batPlayer1.push(newPosUpPlayer1);
+                        if (batPlayer1.length > batLength) {
+                            batPlayer1.shift();
+                        }
+                    } else {
+                        batPlayer1.unshift(newPosPlayer1);
+                        if (batPlayer1.length > batLength) {
+                            batPlayer1.pop();
+                        }
+                    }
+                }
+            } else if(newPosPlayer2.x >= 0 && newPosPlayer2.x <= arenaHeight - cellSize && newPosUpPlayer2.x >= 0 && newPosUpPlayer2.x <= arenaHeight - cellSize) {
+                if (dx !== 0) {
+                    if (dx > 0) {
+                        batPlayer1.push(newPosUpPlayer2);
+                        if (batPlayer1.length > batLength) {
+                            batPlayer1.shift();
+                        }
+                    } else {
+                        batPlayer1.unshift(newPosPlayer2);
+                        if (batPlayer1.length > batLength) {
+                            batPlayer1.pop();
+                        }
+                    }
+                }
+            }
+        }
     }
+    
+    
 
     function createDiv(x, y, className) {
         const gameDiv = document.createElement('div');
@@ -59,48 +104,45 @@ document.addEventListener('DOMContentLoaded', function() {
         batPlayer1.forEach((batCell) => {
             const batElement = createDiv(batCell.x, batCell.y, 'bat');
             gameArena.appendChild(batElement);
-        })
+        });
 
         batPlayer2.forEach((batCell) => {
             const batElement = createDiv(batCell.x, batCell.y, 'bat');
             gameArena.appendChild(batElement);
-        })
+        });
 
-
-        const ballElement =  createDiv(ball.x, ball.y, 'ball');
+        const ballElement = createDiv(ball.x, ball.y, 'ball');
         gameArena.appendChild(ballElement);
-
     }
 
     function gameLoop() {
         setInterval(() => {
-            drawBatAndBall();
             updateBat();
-        }, 100)
-        
+            drawBatAndBall();
+            
+        }, 50);
     }
 
     function gameStarted() {
-        if(!gamestarted) {
+        if (!gamestarted) {
             gamestarted = true;
             gameLoop();
             document.addEventListener('keydown', changeDirection);
+            document.addEventListener('keyup', stopBat); 
         }
     }
 
     function initiateGame() {
         const startButton = document.createElement('button');
-        startButton.textContent = "Start Game";
+        startButton.textContent = 'Start Game';
         startButton.classList.add('start-btn');
         document.body.appendChild(startButton);
 
-        startButton.addEventListener('click', function() {
+        startButton.addEventListener('click', function () {
             startButton.style.display = 'none';
-            
             gameStarted();
-        })
-
+        });
     }
 
     initiateGame();
-})
+});
